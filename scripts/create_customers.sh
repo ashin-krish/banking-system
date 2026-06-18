cd8ad50a02c029ed7edb4d0300e91c93137b1a3b
ashin-krish <ashinkrish066@gmail.com>
2026-06-19 03:40:05 +0530

    added Persistence to the Customer Data

--- scripts/create_customers.sh
+++ scripts/create_customers.sh
@@ -1,13 +1,22 @@
-echo " Loading 10 customers "
+echo "Loading 10 customers..."
 
 for i in {1..10}
-do 
-    name=user$i
-    email=user$i@gmil.com
-    phone=11111111$i
-    address=home$in
-    dob=12-06-202$i
+do
+    name="user$i"
+    email="user$i@gmail.com"
+    phone="11111111$i"
+    address="home$i"
+    dob="12-06-202$i"
 
+    java ui.TestCustomer create "$name" "$email" "$phone" "$address" "$dob"
 
+    if [[ $? -eq 0 ]]
+    then
+        echo "Run Successful for user$i"
+    else
+        echo "Run Failed for user$i"
+    fi
+
+done
 
 
--- ui/TestCustomer.java
+++ ui/TestCustomer.java
@@ -1,46 +1,39 @@
 package ui;
 
-
 import model.Customer;
-
 import service.CustomerService;
 
-public class TestCustomer 
-{
-     public static void main(String[] args) {
+public class TestCustomer {
 
-        // ✅ safety check
+    public static void main(String[] args) {
+
+        // format: create name email phone address dob
         if (args.length < 6) {
-            System.out.println("Usage: create <accountNumber> <name> <balance>");
+            System.out.println("Usage: create <name> <email> <phone> <address> <dob>");
             return;
         }
 
         String command = args[0];
 
-        CustomerService service = new CustomerService();
-
-        // ❗ only handle "create" command for now
         if (!command.equals("create")) {
             System.out.println("Invalid command");
             return;
         }
 
         try {
-           
-            String name = args[0];
-            String email = args[1];
-            String phone = args[2];
-            String address = args[3];
-            String dob = args[4];
+            CustomerService service = new CustomerService();
+
+            String name = args[1];
+            String email = args[2];
+            String phone = args[3];
+            String address = args[4];
+            String dob = args[5];
 
             Customer customer = new Customer(name, email, phone, address, dob);
 
             service.addCustomer(customer);
 
-            System.out.println(" Customer Created succesfully " + customer.getCustomerId());
-            
-
-       
+            System.out.println("Customer created successfully: " + customer.getCustomerId());
 
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
